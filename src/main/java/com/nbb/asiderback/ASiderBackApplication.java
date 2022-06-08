fc35a075060949bf72ba27dae02b64fcd7e4ab78
dhaud <dhaudgkr@gmail.com>
2022-06-08 22:32:10 +0900

    project init

---
+++ src/main/java/com/nbb/asiderback/ASiderBackApplication.java
@@ -0,0 +1,13 @@
+package com.nbb.asiderback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASiderBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ASiderBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nbb/asiderback/ASiderBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbb.asiderback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASiderBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
